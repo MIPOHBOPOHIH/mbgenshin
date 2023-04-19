4b0317bc24ad0b6976483d1dafcaf4b7f8b26edf
MIPOHBOPOHIH <98014624+MIPOHBOPOHIH@users.noreply.github.com>
2023-04-19 16:25:07 +0300

    3

--- main.py
+++ main.py
@@ -18,39 +18,24 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-t", "--template", default="template.html", type=pathlib.Path)
 parser.add_argument("-o", "--output", default="stats.html", type=pathlib.Path)
 parser.add_argument("-c", "--cookies", default=None)
-parser.add_argument("-l", "--lang", "--language", choices=genshin.LANGS, default="ru-ru")
+parser.add_argument("-l", "--lang", "--language", choices=genshin.LANGS, default="en-us")
 
 
 def format_date(date: "datetime"):
-    tz = pytz.timezone("Europe/Moscow")
-    now = date.now(tz=tz)
-    fmt = f"{now.strftime('%b')} \
-            {now.strftime('%d')}, \
-            {now.strftime('%Y')} \
-            {now.strftime('%H:%M %z')}"
-    return fmt
-
-
-async def main():
-    args = parser.parse_args()
-
-    # type: <class 'str'>
-    _c = os.getenv("COOKIES")
-    # must loads to dict
-    cookies = json.loads(_c)
+	@@ -41,16 +41,16 @@ async def main():
 
     client = genshin.Client(cookies, debug=False, game=genshin.Game.GENSHIN)
 
-    user = await client.get_full_genshin_user(0, lang="en-us")
+    user = await client.get_full_genshin_user(0, lang=args.lang)
     abyss = user.abyss.current if user.abyss.current.floors else user.abyss.previous
     diary = await client.get_diary()
 
     try:
-        await client.claim_daily_reward(lang="ru-ru", reward=False)
+        await client.claim_daily_reward(lang=args.lang, reward=False)
     except genshin.AlreadyClaimed:
         pass
     finally:
-        reward = await client.claimed_rewards(lang="ru-ru").next()
+        reward = await client.claimed_rewards(lang=args.lang).next()
         reward_info = await client.get_reward_info()
 
     template: jinja2.Template = jinja2.Template(args.template.read_text())
